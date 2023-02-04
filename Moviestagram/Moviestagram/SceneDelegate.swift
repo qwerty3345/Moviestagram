2a81584eb7e87ba3f9ab81a0c5084065b4c2c91f
qwerty3345 <qwerty3345@naver.com>
2023-02-04 10:52:38 +0900

    feat: 프로젝트 초기 생성

---
+++ Moviestagram/Moviestagram/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Moviestagram
+//
+//  Created by Mason Kim on 2023/02/04.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.overrideUserInterfaceStyle = .light
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Moviestagram/Moviestagram/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Moviestagram
+//
+//  Created by Mason Kim on 2023/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
