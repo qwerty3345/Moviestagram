63a7578421f7d66bab5a08944d8a833965367360
qwerty3345 <qwerty3345@naver.com>
2023-02-05 11:58:22 +0900

    feat: 프로필 헤더 요소 추가

--- Moviestagram/Moviestagram/View/ProfileHeaderView.swift
+++ Moviestagram/Moviestagram/View/ProfileHeaderView.swift
@@ -8,6 +8,41 @@
 import UIKit
 
 final class ProfileHeader: UICollectionReusableView {
+
+    // MARK: - UI Properties
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .lightGray
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textColor = .black
+        return label
+    }()
+
+    private let ratingsLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let wishlistLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        return label
+    }()
+
+
+
+
+    // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemBlue
