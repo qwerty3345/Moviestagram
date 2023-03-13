dad47bf7504183a8ed74700b7607cc608c644aa7
Mason Kim <qwerty3345@naver.com>
2023-03-13 21:03:44 +0900

    refactor: Movie Model의 불필요한 Property 삭제

--- Moviestagram/Moviestagram/Model/Movie.swift
+++ Moviestagram/Moviestagram/Model/Movie.swift
@@ -11,23 +11,17 @@ struct Movie: Codable {
     let id: Int?
     let url: String?
     let title: String?
-    let titleLong: String?
     let summary: String?
     let backgroundImage: String?
     let mediumCoverImage: String?
-    let runtime: Int?
     let rating: Double?
     let year: Int?
     var myRating: Float?
 
     enum CodingKeys: String, CodingKey {
         case id, url, title, summary
-        case titleLong = "title_long"
         case backgroundImage = "background_image"
         case mediumCoverImage = "medium_cover_image"
-        case runtime
-        case rating
-        case year
-        case myRating
+        case rating, year, myRating
     }
 }
